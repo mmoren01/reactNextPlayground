974ed5c5b8401872b7089976b4e79176e2b952fc
Mauricio Moreno <mmoren01@protonmail.com>
2025-10-21 11:38:29 -0400

    Rename component file

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
-import GitHubActivity from '../components/GitHubActivity';
+import GitHubActivity from '../components/GitHub-Activity';
 import Contact from '../components/Contact';
 
 export default function Home() {
